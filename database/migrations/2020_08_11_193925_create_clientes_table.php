55fef4f87b20d1348586d15c9f0d6c23d4e4832d
cchura94 <cchura.cpc@gmail.com>
2020-08-11 20:11:15 -0400

    migraciones en laravel

---
+++ database/migrations/2020_08_11_193925_create_clientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombres", 50);
+            $table->string("apellidos", 50);
+            $table->string("empresa", 50);
+            $table->string("correo", 50)->unique()->nullable();
+            $table->string("telefono", 12);
+            $table->string("ci_nit", 12);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
